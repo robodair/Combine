c11aa1878a752c496088894122de89c43957695f
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-04-29 19:47:53 +1000

    Add 3 basic buttons to the HomeScreen

--- Combine/States/HomeScreen.cs
+++ Combine/States/HomeScreen.cs
@@ -2,20 +2,48 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 using RC_Framework;
 
 namespace Combine
 {
 	public class HomeScreen : RC_GameStateParent
 	{
+		Texture2D buttonTexture;
+		GUI_Control HomeMenu;
 		public HomeScreen(RC_GameStateManager lm) :
 			base(lm)
 		{
 		}
 
+		public override void LoadContent()
+		{
+			buttonTexture = Content.Load<Texture2D>("textures/gui/buttonDefault");
+		}
+
+		public override void EnterLevel(int fromLevelNum)
+		{
+			HomeMenu = new GUI_Control();
+			HomeMenu.AddControl(new ButtonSI(buttonTexture, Color.Blue, new Vector2(200, 100)));
+			HomeMenu.AddControl(new ButtonSI(buttonTexture, Color.Blue, new Vector2(200, 200)));
+			HomeMenu.AddControl(new ButtonSI(buttonTexture, Color.Blue, new Vector2(200, 300)));
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			HomeMenu.Update(gameTime);
+			if (previousMouseState.LeftButton == ButtonState.Released && currentMouseState.LeftButton == ButtonState.Pressed)
+			{
+				HomeMenu.MouseDownEventLeft(currentMouseState.X, currentMouseState.Y);
+			}
+			base.Update(gameTime);
+		}
+
 		public override void Draw(GameTime gameTime)
 		{
 			graphicsDevice.Clear(Color.DarkSlateBlue);
+
+			HomeMenu.drawSubControls(spriteBatch);
 		}
 	}
 }
