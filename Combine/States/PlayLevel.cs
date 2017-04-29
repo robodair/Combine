653c93d26ace219f369d082fa7f194e5d572073f
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-04-29 20:34:49 +1000

    Add a new class to represent a play level of the game

---
+++ Combine/States/PlayLevel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using RC_Framework;
+namespace Combine
+{
+	public class PlayLevel : RC_GameStateParent
+	{
+		public PlayLevel(RC_GameStateManager lm) :
+			base(lm)
+		{
+		}
+
+		public override void LoadContent()
+		{
+			base.LoadContent();
+		}
+
+		public override void EnterLevel(int fromLevelNum)
+		{
+			base.EnterLevel(fromLevelNum);
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
