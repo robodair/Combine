f7eed1ae6b7c219bc6a42e1671dfdc9295d59c8e
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-04-29 21:29:59 +1000

    Add simple move and score displays to PlayLevel

--- Combine/States/PlayLevel.cs
+++ Combine/States/PlayLevel.cs
@@ -8,6 +8,15 @@ namespace Combine
 {
 	public class PlayLevel : RC_GameStateParent
 	{
+		const int rightSideItemsX = 720;
+		const int rightSideStep = 25;
+		Vector2[] RightSideItems = {new Vector2(rightSideItemsX, rightSideStep),
+									new Vector2(rightSideItemsX, rightSideStep * 2),
+									new Vector2(rightSideItemsX, rightSideStep * 3),
+									new Vector2(rightSideItemsX, rightSideStep * 4),
+									new Vector2(rightSideItemsX, rightSideStep * 5)};
+		int move;
+		int score;
 		public PlayLevel(RC_GameStateManager lm) :
 			base(lm)
 		{
@@ -20,6 +29,8 @@ namespace Combine
 
 		public override void EnterLevel(int fromLevelNum)
 		{
+			move = 0;
+			score = 0;
 			base.EnterLevel(fromLevelNum);
 		}
 
@@ -31,6 +42,11 @@ namespace Combine
 		public override void Draw(GameTime gameTime)
 		{
 			graphicsDevice.Clear(Color.DarkSlateBlue);
+
+			spriteBatch.DrawString(font, "MOVE", RightSideItems[0], Color.Gray, 0, Vector2.Zero, 0.3f, SpriteEffects.None, 0);
+			spriteBatch.DrawString(font, move.ToString(), RightSideItems[1], Color.White, 0, Vector2.Zero, 0.3f, SpriteEffects.None, 0);
+			spriteBatch.DrawString(font, "SCORE", RightSideItems[2], Color.Gray, 0, Vector2.Zero, 0.3f, SpriteEffects.None, 0);
+			spriteBatch.DrawString(font, score.ToString(), RightSideItems[3], Color.White, 0, Vector2.Zero, 0.3f, SpriteEffects.None, 0);
 		}
 
 	}
