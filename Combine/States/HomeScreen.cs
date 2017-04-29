83b65f2c9a821d90409c059875285cbca9064e40
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-04-29 17:12:10 +1000

    Add a class for a home screen

---
+++ Combine/States/HomeScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using RC_Framework;
+
+namespace Combine
+{
+	public class HomeScreen : RC_GameStateParent
+	{
+		public HomeScreen(GraphicsDevice g, SpriteBatch s, ContentManager c, RC_GameStateManager lm) :
+			base(g, s, c, lm)
+		{
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			graphicsDevice.Clear(Color.DarkSlateBlue);
+		}
+	}
+}
