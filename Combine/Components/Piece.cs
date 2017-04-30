cb5c9537b7cda118f94e42e546b95e2f6fe677d8
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-04-30 12:04:59 +1000

    Autogeneration of Tetrominoes (Square Pieces)

--- Combine/Components/Piece.cs
+++ Combine/Components/Piece.cs
@@ -10,19 +10,103 @@ namespace Combine
 	{
 		Vector2 Position;
 		Vector2 TargetPosition;
-		public bool inPosition { get; set;}
+		public bool inPosition { get; set; }
 		int moveSpeed = 5;
+		const int partSize = 30;
+		const int partSpacing = 5;
 
 		Sprite3[] parts;
 
-		public Piece(float x)
+		Random rand;
+
+		public Piece(float x, String type)
 		{
+			rand = new Random();
 			inPosition = false;
 			Position = new Vector2(x, -50); // start above the game
-			parts = new Sprite3[1]; // will be variable length later
-			parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y);
-			parts[0].setWidthHeight(10, 10);
-			parts[0].setHSoffset(Vector2.Zero);
+			createPieces(type);
+		}
+
+		private void createPieces(String type)
+		{
+			switch (type)
+			{
+				case "square":
+					parts = new Sprite3[4];
+					createTetrominoParts(rand.Next(0, 7)); // New square piece (Tetromino)
+					break;
+				case "triangle":
+					throw new NotImplementedException();
+					break;
+				case "pentagon":
+					throw new NotImplementedException();
+					break;
+			}
+		}
+
+		private void createTetrominoParts(int layout)
+		{
+			// set the part hotspot offsets
+			switch (layout)
+			{
+				case 0:
+					// I block
+					parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y);
+					parts[1] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y);
+					parts[2] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 2, Position.Y);
+					parts[3] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 3, Position.Y);
+					break;
+				case 1:
+					// O block
+					parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y);
+					parts[1] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y);
+					parts[2] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y + partSize + partSpacing);
+					parts[3] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y + partSize + partSpacing);
+					break;
+				case 2:
+					// T Block
+					parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y);
+					parts[1] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y);
+					parts[2] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 2, Position.Y);
+					parts[3] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y + partSize + partSpacing);
+					break;
+				case 3:
+					// J block
+					parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y);
+					parts[1] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y);
+					parts[2] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 2, Position.Y);
+					parts[3] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 2, Position.Y + partSize + partSpacing);
+					break;
+				case 4:
+					// L block
+					parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y);
+					parts[1] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y);
+					parts[2] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 2, Position.Y);
+					parts[3] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y + partSize + partSpacing);
+					break;
+				case 5:
+					// S block
+					parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y);
+					parts[1] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 2, Position.Y);
+					parts[2] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y + partSize + partSpacing);
+					parts[3] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y + partSize + partSpacing);
+					break;
+				case 6:
+					// Z block
+					parts[0] = new Sprite3(true, UtilTexSI.texWhite, Position.X, Position.Y);
+					parts[1] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y);
+					parts[2] = new Sprite3(true, UtilTexSI.texWhite, Position.X + partSize + partSpacing, Position.Y + partSize + partSpacing);
+					parts[3] = new Sprite3(true, UtilTexSI.texWhite, Position.X + (partSize + partSpacing) * 2, Position.Y + partSize + partSpacing);
+					break;
+			}
+
+			// Color the parts
+			foreach (Sprite3 part in parts)
+			{
+				part.setWidthHeight(partSize, partSize);
+				part.setColor(Color.Yellow);
+
+			}
 		}
 
 		public void SetTargetPosition(Vector2 targetPosition)
