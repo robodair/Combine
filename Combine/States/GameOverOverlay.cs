1e11b7d1cd0c1ff9879a9405adddc47118d65529
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-05-10 01:59:17 +1000

    Add Help and Game Over Screens

---
+++ Combine/States/GameOverOverlay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using RC_Framework;
+namespace Combine
+{
+	public class GameOverOverlay : RC_GameStateParent
+	{
+		Rectangle pauseBackground;
+		Vector2 textPos;
+		Song gameOverSong;
+
+		public GameOverOverlay(RC_GameStateManager lm) :
+							base(lm)
+		{
+			pauseBackground = new Rectangle(graphicsManager.PreferredBackBufferWidth / 2 - 100,
+										   graphicsManager.PreferredBackBufferHeight / 2 - 100,
+											200, 100);
+			textPos = new Vector2(graphicsManager.PreferredBackBufferWidth / 2 - 80,
+									   graphicsManager.PreferredBackBufferHeight / 2 - 60);
+		}
+
+		public override void EnterLevel(int fromLevelNum)
+		{
+			// Play the game over song
+			MediaPlayer.Play(gameOverSong);
+			base.EnterLevel(fromLevelNum);
+		}
+
+		public override void LoadContent()
+		{
+			gameOverSong = Content.Load<Song>("music/doodle");
+			base.LoadContent();
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			spriteBatch.Draw(UtilTexSI.texWhite, pauseBackground, Color.Lavender);
+			spriteBatch.DrawString(font, "Game Over", textPos, Color.Black, 0, Vector2.Zero, 0.5f, SpriteEffects.None, 0);
+		}
+
+	}
+}
---
+++ Combine/States/HelpScreen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using RC_Framework;
+namespace Combine
+{
+	public class HelpScreen : RC_GameStateParent
+	{
+		Vector2 textPos;
+		Song gameOverSong;
+
+		String HelpText = $@"Help for Combine:
+================={Environment.NewLine}
+Combine is a sweet and simple game.{Environment.NewLine}
+Use your mouse to drag and drop pieces on to
+the grid to make different shapes!{Environment.NewLine}
+Making shapes gets you more points,
+see what you can get your highscore to for the different grid types.{ Environment.NewLine }
+F1 to exit help
+================={Environment.NewLine}";
+
+		public HelpScreen(RC_GameStateManager lm) :
+							base(lm)
+		{
+			textPos = new Vector2(80, 60);
+		}
+
+		public override void EnterLevel(int fromLevelNum)
+		{
+			// Play the game over song
+			MediaPlayer.Play(gameOverSong);
+			base.EnterLevel(fromLevelNum);
+		}
+
+		public override void LoadContent()
+		{
+			gameOverSong = Content.Load<Song>("music/doodle");
+			base.LoadContent();
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			graphicsDevice.Clear(Color.Lavender);
+			spriteBatch.DrawString(font, HelpText, textPos, Color.DarkSlateGray, 0, Vector2.Zero, 0.3f, SpriteEffects.None, 0);
+		}
+
+	}
+}
